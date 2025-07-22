c4c522a31397a971a26ce4ed4aca5ea68ae418b1
snorle01 <snorle01@yahoo.com>
2025-07-22 22:43:17 +0200

    load from existing level done

--- models/square_model.py
+++ models/square_model.py
@@ -1,5 +1,4 @@
 from engine.model import *
-from engine.rect import rect_class
 from vbo.square_vbo import square_frame_VBO, square_VBO
 
 from typing import TYPE_CHECKING
@@ -7,14 +6,13 @@ if TYPE_CHECKING:
     from engine.engine import engine_class
 
 class square_base_model(base_model):
-    def __init__(self, engine, position: tuple[int, int, int], scale: tuple[int, int, int], rect_position, rect_width, rect_height, color: glm.vec3 = glm.vec3(1.0, 0.0, 0.0)):
+    def __init__(self, engine, position: tuple[int, int, int], scale: tuple[int, int, int], color: glm.vec3 = glm.vec3(1.0, 0.0, 0.0)):
         super().__init__(engine)
         self.engine: engine_class = engine
         self.position = position
         self.scale = scale
         self.m_model = self.get_model_matrix()
         self.color = color
-        self.rect = rect_class(rect_position, rect_width, rect_height)
 
     def get_model_matrix(self):
         m_model = glm.mat4()
@@ -29,8 +27,8 @@ class square_base_model(base_model):
         return m_model
 
 class square_frame_model(square_base_model):
-    def __init__(self, engine, position, scale, rect_position, rect_width, rect_height, color = glm.vec3(1, 0, 0)):
-        super().__init__(engine, position, scale, rect_position, rect_width, rect_height, color)
+    def __init__(self, engine, position, scale, color = glm.vec3(1, 0, 0)):
+        super().__init__(engine, position, scale, color)
         self.get_VAO(square_frame_VBO(self.engine.ctx), "color", "test")
 
     def render(self):
@@ -39,8 +37,8 @@ class square_frame_model(square_base_model):
         self.VAO.render(moderngl.LINE_LOOP)
 
 class square_model(square_base_model):
-    def __init__(self, engine, position, scale, rect_position, rect_width, rect_height, color = glm.vec3(1, 0, 0)):
-        super().__init__(engine, position, scale, rect_position, rect_width, rect_height, color)
+    def __init__(self, engine, position, scale, color = glm.vec3(1, 0, 0)):
+        super().__init__(engine, position, scale, color)
         self.get_VAO(square_VBO(self.engine.ctx), "color", "test")
 
     def render(self):
--- scenes/test_scene.py
+++ scenes/test_scene.py
@@ -13,7 +13,7 @@ class test_scene(base_scene):
         self.ceiling_text = text_model(self.engine, "ceiling", (-1, 0.9, 0), (0.1, 0.1, 1))
         self.text_layer = [self.floor_text, self.wall_text, self.ceiling_text]
 
-        self.grid = (20, 30)
+        self.grid = (10, 10)
         
         self.squares_floors: List[square_model] = []
         self.create_squares(self.squares_floors)
@@ -38,55 +38,48 @@ class test_scene(base_scene):
             value_list.append(0)
 
     def create_squares(self, squares_list: List[square_model]):
-        square_width = 2 / self.grid[0]
-        square_height = 2 / self.grid[1]
-        if square_width < square_height:
-            square_size = square_width
-        else:
-            square_size = square_height
+        screen_width = pygame.display.get_window_size()[0]
+        screen_height = pygame.display.get_window_size()[1]
 
-        rect_width = pygame.display.get_window_size()[0] / self.grid[0]
-        rect_height = pygame.display.get_window_size()[1] / self.grid[1]
-        if rect_width < rect_height:
-            rect_size = rect_width
-        else:
-            rect_size = rect_height
+        square_width = (2 * (screen_height / screen_width)) / self.grid[0]
+        square_height = 2 / self.grid[1]
 
         square_y = -1
-        rect_y = pygame.display.get_window_size()[1] - rect_height
         for yi in range(self.grid[1]):
             square_x = -1
-            rect_x = 0
             for xi in range(self.grid[0]):
-                squares_list.append(square_model(self.engine, (square_x, square_y, 0), (square_width, square_height, 1), (rect_x, rect_y), rect_width, rect_height))
-                self.add_object(square_frame_model(self.engine, (square_x, square_y, 0), (square_width, square_height, 1), (rect_x, rect_y), rect_width, rect_height, glm.vec3(0.0, 0.0, 0.0)))
+                squares_list.append(square_model(self.engine, (square_x, square_y, 0), (square_width, square_height, 1)))
+                self.add_object(square_frame_model(self.engine, (square_x, square_y, 0), (square_width, square_height, 1), glm.vec3(0.0, 0.0, 0.0)))
                 square_x += square_width
-                rect_x += rect_width
             
             square_y += square_height
-            rect_y -= rect_height
 
     def event(self, event:pygame.event.Event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            on_color = glm.vec3(0.0, 1.0, 0.0)
-            off_color = glm.vec3(1.0, 0.0, 0.0)
 
-            x = int(mouse_pos[0] // (pygame.display.get_window_size()[0] / self.grid[0]))
-            fliped_y = int(mouse_pos[1] // (pygame.display.get_window_size()[1] / self.grid[1]))
-            y = (self.grid[1] - 1) - fliped_y
-            index = x + (y * self.grid[0])
+            screen_width = pygame.display.get_window_size()[0]
+            screen_height = pygame.display.get_window_size()[1]
 
-            square_layer = self.squares_layers[self.layer_index]
-            value_layer = self.values_layers[self.layer_index]
-            square = square_layer[index]
+            if mouse_pos[0] <= screen_height:
+                on_color = glm.vec3(0.0, 1.0, 0.0)
+                off_color = glm.vec3(1.0, 0.0, 0.0)
 
-            if value_layer[index] == 0:
-                value_layer[index] = 1
-                square.color = on_color
-            else:
-                value_layer[index] = 0
-                square.color = off_color
+                x = int(mouse_pos[0] // (screen_height / self.grid[0]))
+                fliped_y = int(mouse_pos[1] // (screen_height / self.grid[1]))
+                y = (self.grid[1] - 1) - fliped_y
+                index = x + (y * self.grid[0])
+
+                square_layer = self.squares_layers[self.layer_index]
+                value_layer = self.values_layers[self.layer_index]
+                square = square_layer[index]
+
+                if value_layer[index] == 0:
+                    value_layer[index] = 1
+                    square.color = on_color
+                else:
+                    value_layer[index] = 0
+                    square.color = off_color
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP: # up
@@ -101,6 +94,7 @@ class test_scene(base_scene):
 
             if event.key == pygame.K_s: # save
                 dictionary = {
+                    "size": self.grid,
                     "floors": self.values_floor,
                     "walls": self.values_walls,
                     "ceiling": self.values_ceiling
@@ -112,6 +106,35 @@ class test_scene(base_scene):
                 # Writing to sample.json
                 with open("level_data.json", "w") as outfile:
                     outfile.write(json_object)
+                
+            if event.key == pygame.K_l: # load
+                on_color = glm.vec3(0.0, 1.0, 0.0)
+                off_color = glm.vec3(1.0, 0.0, 0.0)
+
+                with open('level_data.json', 'r') as file:
+                    data = json.load(file)
+
+                    self.values_floor = data["floors"]
+                    self.values_walls = data["walls"]
+                    self.values_ceiling = data["ceiling"]
+
+                    for index in range(self.grid[0] * self.grid[1]):
+                        if self.values_floor[index] == 0:
+                            self.squares_floors[index].color = off_color
+                        else:
+                            self.squares_floors[index].color = on_color
+
+                    for index in range(self.grid[0] * self.grid[1]):
+                        if self.values_walls[index] == 0:
+                            self.squares_walls[index].color = off_color
+                        else:
+                            self.squares_walls[index].color = on_color
+
+                    for index in range(self.grid[0] * self.grid[1]):
+                        if self.values_ceiling[index] == 0:
+                            self.squares_ceiling[index].color = off_color
+                        else:
+                            self.squares_ceiling[index].color = on_color
 
     def render(self):
         super().render()
